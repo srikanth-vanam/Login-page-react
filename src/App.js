85d9877cfba4552804f5abcbd7aaeaf11a10c911
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-12 19:58:25 +0530

    solved user-logout issue on page refresh via useEffect()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, {  useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,14 +6,23 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+  // to solve the user-logout on page refresh
+  useEffect(()=>{
+    const userLogged=localStorage.getItem("isLoggedIn");
+    if(userLogged === "1"){
+      setIsLoggedIn(true);
+    }
+  },[])
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn","1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
